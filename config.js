742ee03075ba0e0acfc8009ec6803da3803a4802
Alulaexperience <konka19111993@gmail.com>
2025-01-07 09:57:01 +0300

    config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+**Usage:**
+
+Use the base URL: https://api.rcu.gov.sa/weather.
+Example request
+
+
+### Headers:
+```plaintext
+GET /e-weather/v9/forecastdb?profile=alularcu HTTP/1.1
+Host: api.rcu.gov.sa
+Sec-Ch-Ua-Platform: "Mac"
+Accept-Language: en-US,en;q=0.9
+Accept: application/json, text/plain, */*
+Sec-Ch-Ua: "Chromium";v="131", "Not_A Brand";v="24"
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Safari/537.36
+Sec-Ch-Ua-Mobile: ?0
+Sec-Fetch-Site: cross-site
+Sec-Fetch-Mode: cors
+Sec-Fetch-Dest: empty
+Accept-Encoding: gzip, deflate, br
+Priority: u=1, i
+Connection: keep-alive
+
